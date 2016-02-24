f8d769296737537cac38536df7654fbf9440a8ba
russellgoldenberg <russellgoldenberg@gmail.com>
2016-02-24 11:34:34 -0500

    tweaks

--- src/dev/js/urlManager.js
+++ src/dev/js/urlManager.js
@@ -1,13 +1,12 @@
-export default function urlManager(opts) {
-
-	const { level, date, test } = opts
+export default function urlManager({ level, date, test }) {
 
 	// construct the api url
-	const baseUrl = test 
+	const baseUrl = test
 		? '//dev.apps.bostonglobe.com/electionapi/elections/'
 		: '//www.bostonglobe.com/electionapi/elections/'
 
 	const url = `${baseUrl}${date}?&level=${level}&officeID=P`
 
 	return url
+
 }
