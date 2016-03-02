c125e5de4e7e3aa57c3ca43e87a708e4e21a608f
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-01 22:08:16 -0500

    polyfill

--- src/dev/js/dev.js
+++ src/dev/js/dev.js
@@ -7,6 +7,32 @@ import orderBy from 'lodash.orderby'
 import urlManager from './urlManager'
 import dom from './dom'
 
+if (console && console.log) {
+	console.log('super tuesday');
+}
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this === null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
+
 function toPercent(x, shorten) {
 
 	const decimalPlaces = shorten ? 0 : 1
